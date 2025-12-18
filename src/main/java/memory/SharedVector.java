40d24ca573b1c9e55429dbf2a646d84afb8e002c
yagelx101 <yagelx101@example.com>
2025-12-18 21:42:53 +0200

    add methods in vector class

--- src/main/java/memory/SharedVector.java
+++ src/main/java/memory/SharedVector.java
@@ -10,21 +10,23 @@ public class SharedVector {
 
     public SharedVector(double[] vector, VectorOrientation orientation) {
         // TODO: store vector data and its orientation
+        this.vector = vector;
+        this.orientation = orientation;
     }
 
     public double get(int index) {
         // TODO: return element at index (read-locked)
-        return 0;
+        return vector[index];
     }
 
     public int length() {
         // TODO: return vector length
-        return 0;
+        return vector.length;
     }
 
     public VectorOrientation getOrientation() {
         // TODO: return vector orientation
-        return null;
+        return orientation;
     }
 
     public void writeLock() {
@@ -45,19 +47,47 @@ public class SharedVector {
 
     public void transpose() {
         // TODO: transpose vector
+        if (orientation == VectorOrientation.COLUMN_MAJOR){
+            orientation = VectorOrientation.ROW_MAJOR;
+        }
+        else{
+            orientation = VectorOrientation.COLUMN_MAJOR;
+        }
     }
 
     public void add(SharedVector other) {
         // TODO: add two vectors
+        if (this.length() != other.length()){
+            throw new IllegalArgumentException("vectors must be in the same size");
+        }
+        if (this.getOrientation() != other.getOrientation()){
+            throw new IllegalArgumentException("vectors must be in the same orientation");
+        }
+        for (int i=0; i<this.length(); i++){
+            this.vector[i] += other.get(i);
+        }
     }
 
     public void negate() {
         // TODO: negate vector
+        for (int i=0; i<this.length(); i++){
+            this.vector[i] *= -1;
+        }
     }
 
     public double dot(SharedVector other) {
         // TODO: compute dot product (row · column)
-        return 0;
+        if (this.getOrientation() == other.getOrientation()){
+            throw new IllegalArgumentException("");
+        }
+        if (this.length() != other.length()){
+            throw new IllegalArgumentException("vectors must be in the same size");
+        }
+        double sum = 0;
+        for (int i=0; i<length(); i++){
+            sum = sum + (this.get(i) * other.get(i));
+        }
+        return sum;
     }
 
     public void vecMatMul(SharedMatrix matrix) {
